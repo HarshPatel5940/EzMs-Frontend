8fe67cdd59a2425089eff2a29d7f96dac05aebed
Harsh Patel <harshnpatel9@gmail.com>
2023-12-26 23:34:27 +0530

    chore: add missing import statement

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '@/components/navbar';
 import { Button } from '@/components/ui/button';
